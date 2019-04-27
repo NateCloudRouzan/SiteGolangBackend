222214abc2bccab7422f63b9a799cf45d7f5d3f7
NateCloudRouzan <cloudrouzan@gmail.com>
2019-04-27 11:44:44 -0700

    Trying to implement a form and a template
    We will see if it goes well live

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"net/http"
+    "html/template"
 )
 
 func init() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
+    
+    tpl = template.Must(template.ParseFiles("templatePractice.gohtml"))
+    
+    err := tpl.Execute(os.Stdout, "From GolangFile")
+    if err != nil{
+        log.Fatlln(err)
+    }
 }

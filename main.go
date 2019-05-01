8d685446e2f59a449166feccc55b667abd0ca246
NateCloudRouzan <cloudrouzan@gmail.com>
2019-05-01 12:40:54 -0700

    Maybe get a 404 Custom Handler
    Knockin Shiii out. stuff is rollin

--- main.go
+++ main.go
@@ -7,13 +7,39 @@ import (
 )
 
 
-func handler(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Path: %s!", r.URL.Path[1:])
+
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        errorHandler(w, r, http.StatusNotFound)
+        return
+    }
+    fmt.Fprint(w, "welcome home")
+}
+
+func smthHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/smth/" {
+        errorHandler(w, r, http.StatusNotFound)
+        return
+    }
+    fmt.Fprint(w, "welcome smth")
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+    w.WriteHeader(status)
+    if status == http.StatusNotFound {
+        fmt.Fprint(w, "custom 404")
+    }
+}
+
+func handler(w http.ResponseWriter, r *http.Request){ 
+    fmt.Fprintf(w, "Path: %s, Length:", r.URL.Path[1:], len(r.URL.Path[1:]))
 }
 
 func init() {
 	//http.Handle("/", http.FileServer(http.Dir(".")))
-    http.HandleFunc("/", handler)
+        http.HandleFunc("/", homeHandler)
+    http.HandleFunc("/smth/", smthHandler)
     
 //    tpl = template.Must(template.ParseFiles("templatePractice.gohtml"))
     

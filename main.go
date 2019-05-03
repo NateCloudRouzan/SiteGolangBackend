512f512f14f13e23cd83c6efa43ae9d9c1081f2a
NateCloudRouzan <cloudrouzan@gmail.com>
2019-05-03 11:49:22 -0700

    StripPrefix worked!
    were doin it

--- main.go
+++ main.go
@@ -84,6 +84,20 @@ func imageHandler(w http.ResponseWriter, r *http.Request){
  //   http.Handle("/media/img/", http.FileServer(http.Dir(".")))
 }
 
+func templateHandler(w http.ResponseWriter, r *http.Request){
+    tmpl := template.Must(template.ParseFiles("layout.html"))
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		tmpl.Execute(w, data)
+	})
+}
 
 func init() {
 	//http.Handle("/", http.FileServer(http.Dir(".")))
@@ -96,6 +110,8 @@ func init() {
     http.HandleFunc("/css/", cssServer)
     
     http.Handle("/media/img/", http.StripPrefix("/media/img/", http.FileServer(http.Dir("./media/img/"))))
+    http.Handle("/GolangPractice/", http.StripPrefix("/GolangPractice/", http.FileServer(http.Dir("./GolangPractice/"))))
+
 
     //I think strip prefix is the answer
     

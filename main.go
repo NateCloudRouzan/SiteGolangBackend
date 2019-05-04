657f3d8cbe5ec90bb7688aff6a38e33db07f3e6c
NateCloudRouzan <cloudrouzan@gmail.com>
2019-05-04 10:48:58 -0700

    Chuggin through

--- main.go
+++ main.go
@@ -56,6 +56,11 @@ func udemyHandler(w http.ResponseWriter, r *http.Request) {//Should be a portal 
 
 func udemyProjectsHandler(w http.ResponseWriter, r *http.Request) { //Should be a repo of all of my projects
     //    http.Handle("/GolangPractice/", http.StripPrefix("/GolangPractice/", http.FileServer(http.Dir("./GolangPractice/"))))
+    if r.URL.Path != "/GolangPractice/template1" {
+        fmt.Fprint(w, "other shit")
+        return
+    }
+    
     fmt.Fprint(w, "other shit")
 }
                                                          

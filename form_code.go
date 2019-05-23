e61b4ed2258d89c59b77f7918ffd6c3be712a535
NateCloudRouzan <cloudrouzan@gmail.com>
2019-05-22 23:01:24 -0700

    form code broken off now too

---
+++ form_code.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+    "html/template"
+
+
+)
+
+type ContactDetails struct {
+	Email   string
+	Subject string
+	Message string
+}
+
+func form1Handler(w http.ResponseWriter, r *http.Request){
+    http.ServeFile(w , r , "form1.html")
+}
+
+func handlingForm(w http.ResponseWriter, r *http.Request){
+    tmpl := template.Must(template.ParseFiles("form1.html"))
+    if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return
+    }
+
+    details := ContactDetails{
+        Email:   r.FormValue("email"),
+        Subject: r.FormValue("subject"),
+        Message: r.FormValue("message"),
+    }
+
+    // do something with details
+    _ = details
+
+    tmpl.Execute(w, struct{ Success bool }{true})    
+}
+
+func form2Handler(w http.ResponseWriter, r *http.Request){
+    http.ServeFile(w , r , "form2.html")
+}
+
+func form_2(w http.ResponseWriter, r *http.Request){
+       tmpl := template.Must(template.ParseFiles("form2.html"))
+    if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return
+    }
+    
+    c := LoginInfo{
+        Success: true, 
+        Fname: r.FormValue("fname"), 
+        Lname: r.FormValue("lname"),
+        Pword: r.FormValue("pword"),
+    }
+    
+    if c.Fname == "Nate"{
+        c.Authorized = true
+    }
+    
+    tmpl.Execute(w, c)   
+}
+
+func form3Handler(w http.ResponseWriter, r *http.Request){
+    http.ServeFile(w , r , "form3.html")
+}
+
+func form_3(w http.ResponseWriter, r *http.Request){
+    tmpl := template.Must(template.ParseFiles("form3.html"))
+    tmpl.Execute(w, r.FormValue("photo"))        
+}
+
+func form_3_redir(w http.ResponseWriter, r *http.Request){
+    fmt.Fprint(w, r.Method)
+}
--- main.go
+++ main.go
@@ -32,12 +32,6 @@ type TodoPageData struct {
 	Todos     []Todo
 }
 
-type ContactDetails struct {
-	Email   string
-	Subject string
-	Message string
-}
-
 type LoginInfo struct {
 	Success bool
     Authorized bool
@@ -117,69 +111,6 @@ func udemyProjectsHandler(w http.ResponseWriter, r *http.Request) { //Should be 
 
 func iconHandler(w http.ResponseWriter, r *http.Request){
     http.ServeFile(w , r , "cloudIcon.ico")
-}
-
-func form1Handler(w http.ResponseWriter, r *http.Request){
-    http.ServeFile(w , r , "form1.html")
-}
-
-
-
-func handlingForm(w http.ResponseWriter, r *http.Request){
-    tmpl := template.Must(template.ParseFiles("form1.html"))
-    if r.Method != http.MethodPost {
-			tmpl.Execute(w, nil)
-			return
-    }
-
-    details := ContactDetails{
-        Email:   r.FormValue("email"),
-        Subject: r.FormValue("subject"),
-        Message: r.FormValue("message"),
-    }
-
-    // do something with details
-    _ = details
-
-    tmpl.Execute(w, struct{ Success bool }{true})    
-}
-
-func form2Handler(w http.ResponseWriter, r *http.Request){
-    http.ServeFile(w , r , "form2.html")
-}
-
-func form_2(w http.ResponseWriter, r *http.Request){
-       tmpl := template.Must(template.ParseFiles("form2.html"))
-    if r.Method != http.MethodPost {
-			tmpl.Execute(w, nil)
-			return
-    }
-    
-    c := LoginInfo{
-        Success: true, 
-        Fname: r.FormValue("fname"), 
-        Lname: r.FormValue("lname"),
-        Pword: r.FormValue("pword"),
-    }
-    
-    if c.Fname == "Nate"{
-        c.Authorized = true
-    }
-    
-    tmpl.Execute(w, c)   
-}
-
-func form3Handler(w http.ResponseWriter, r *http.Request){
-    http.ServeFile(w , r , "form3.html")
-}
-
-func form_3(w http.ResponseWriter, r *http.Request){
-    tmpl := template.Must(template.ParseFiles("form3.html"))
-    tmpl.Execute(w, r.FormValue("photo"))        
-}
-
-func form_3_redir(w http.ResponseWriter, r *http.Request){
-    fmt.Fprint(w, r.Method)
 }
 
 func studentSealHandler(w http.ResponseWriter, r *http.Request){
